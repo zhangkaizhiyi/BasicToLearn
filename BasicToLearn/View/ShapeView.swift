d1f4efccfe32ef96dd129013ce992821e3d1fb8d
zhangkaizhiyi <1018476287@qq.com>
2021-08-08 00:23:06 +0800

    第二次commit,看看不能切换

--- BasicToLearn/View/ShapeView.swift
+++ BasicToLearn/View/ShapeView.swift
@@ -5,4 +5,20 @@
 //  Created by 张凯 on 2021/8/7.
 //
 
-import Foundation
+import SwiftUI
+
+struct ShapeView: View {
+    
+
+    var body: some View {
+        Text("nihao")
+    }
+
+}
+
+
+struct ShapeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShapeView()
+    }
+}
